cde8ca6a7e5eb0edbf34b152126b1650368beb46
indian-chutney <zubairshaik2615@gmail.com>
2025-08-05 16:24:20 +0530

    auth done

--- dist/middleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { type NextFunction, type Request, type Response } from "express";
-export declare function authenticate(req: Request, res: Response, next: NextFunction): Promise<void>;
-//# sourceMappingURL=middleware.d.ts.map
--- dist/types/supabase.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=supabase.js.map
